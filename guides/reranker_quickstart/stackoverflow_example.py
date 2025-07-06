f28c9262a78614cdc552482d79804d9c82809725
Mahima Manik <mahimamanik.22@gmail.com>
2025-07-06 13:50:57 -0700

    Load stackoverflow data as strings

--- guides/reranker_quickstart/stackoverflow_example.py
+++ guides/reranker_quickstart/stackoverflow_example.py
@@ -1,20 +1,33 @@
 import asyncio
 import json
 import os
-
+import pandas as pd
 import aiohttp
+
+
+from datasets import load_dataset
+
+def documents(dataset_name: str):
+    ds = load_dataset(dataset_name, split="test")
+    df = ds.to_pandas()
+    
+    print(f"Dataset has {len(df)} rows")
+    
+    # Convert all entries to list of strings
+    text_entries = [str(row) for row in df.to_dict('records')]
+    
+    print(f"\nReturning {len(text_entries)} text entries")
+    return text_entries[:500]
 
 
 async def main() -> None:
     headers = {
         "Authorization": f"Bearer {os.environ["ZEROENTROPY_API_KEY"]}",
     }
+    documents_list = documents("mteb/stackoverflowdupquestions-reranking")
     payload = {
-        "query": "What is 2+2?",
-        "documents": [
-            "4",
-            "The answer is definitely 1 million.",
-        ],
+        "query": "How to fix 'AttributeError: 'NoneType' object has no attribute 'lower''?",
+        "documents": documents_list,
     }
 
     async with aiohttp.ClientSession() as client, client.post(
